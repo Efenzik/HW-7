8e0b0cb8a1300685bc054d755ea3815a28c868f2
Artem Cherkashin <157517719+Efenzik@users.noreply.github.com>
2025-02-17 19:41:14 +0200

    ,

---
+++ index.js
@@ -0,0 +1,58 @@
+"use strict";
+//1=====================
+
+// const numbersSeries = [15, 52, 999];
+// numbersSeries[1] = 10;
+// console.log(numbersSeries);
+
+//2=====================
+
+// const fruits = ["Хурма", "Ківі", "Банан"];
+// fruits[3] = "Ананас";
+// console.log(fruits);
+
+//3=====================
+
+// const numbers = [15, 52, 999];
+// let sum = 0;
+// for (let number of numbers) {
+//   sum += number;
+// }
+// console.log(sum);
+
+//4=====================
+
+// const num = [15, 52, 999, 12, 1050];
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+// }
+
+//5=====================
+
+// const logins = ["Efen", "Efenzik", "qqw", "mollystutu", "Efenzik2"];
+// for (let i = 0; i < logins.length; i++) {
+//   if (logins[i].length < 5) {
+//     continue;
+//   }
+//   console.log(logins[i]);
+// }
+
+//6=====================
+
+// const numbers = [15, 52, 999, 12, 1050];
+// let maxNumber = numbers[0];
+// for (let num of numbers) {
+//   if (num > maxNumber) {
+//     maxNumber = num;
+//   }
+// }
+// console.log(maxNumber);
+
+//7=====================
+
+// const numbers = [15, 52, 999, 12, 1050];
+// for (let num of numbers) {
+//   if (num % 2 === 0) {
+//     console.log(num);
+//   }
+// }
